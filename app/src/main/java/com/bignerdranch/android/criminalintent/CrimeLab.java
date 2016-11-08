6b1da62506c640d713c5a6f760cfb7cd07222eeb
jerryzhang1002@gmail.com <jerryzhang1002@gmail.com>
2016-11-08 00:44:21 -0500

    fix bug

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeLab.java
@@ -19,11 +19,17 @@ public class CrimeLab {
         if (sCrimeLab == null) {
             sCrimeLab = new CrimeLab(context);
         }
-        return sCrimeLab
+        return sCrimeLab;
     }
 
     private CrimeLab(Context context) {
         mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved((i % 2 == 0));
+            mCrimes.add(crime);
+        }
     }
 
     public List<Crime> getCrimes() {
