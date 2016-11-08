a88c1ed7eac1c17815135dd5ea7be2bd4fec6f82
jerryzhang1002@gmail.com <jerryzhang1002@gmail.com>
2016-11-08 01:37:20 -0500

    add crimelistfragment

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -1,6 +1,12 @@
 package com.bignerdranch.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by JerryCheung on 11/8/16.
@@ -8,4 +14,16 @@ import android.support.v4.app.Fragment;
 
 public class CrimeListFragment extends Fragment {
 
+    private RecyclerView mCrimeRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate((R.layout.fragment_crime_list, container, false));
+
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 }
