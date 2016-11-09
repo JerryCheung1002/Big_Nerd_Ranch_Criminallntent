bd9a5dc21842b0d0c764d29c7ed9809462e1b26e
jerryzhang1002@gmail.com <jerryzhang1002@gmail.com>
2016-11-09 13:38:49 -0500

    add CrimePagerActivity

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,43 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+
+/**
+ * Created by JerryCheung on 11/9/16.
+ */
+
+public class CrimePagerActivity extends FragmentActivity {
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter() {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+    }
+}
