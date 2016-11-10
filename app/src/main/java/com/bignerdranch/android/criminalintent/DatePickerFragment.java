2425d51705c4b801db0b226b04425eed4f424636
jerryzhang1002@gmail.com <jerryzhang1002@gmail.com>
2016-11-10 16:29:59 -0500

    add datepickerfragment

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,21 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+/**
+ * Created by JerryCheung on 11/10/16.
+ */
+
+public class DatePickerFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
